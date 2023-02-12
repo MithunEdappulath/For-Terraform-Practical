78839655dd7a5a19f9c567688090f72ef44b08f2
MithunEdappulath <120838449+MithunEdappulath@users.noreply.github.com>
2023-02-12 08:52:13 -0500

    IAM Group Creation
    IAM Group Creation

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+  path = "/users/"
+}
